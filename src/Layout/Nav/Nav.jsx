b7cdfde3da3d195715bad34c3ef7109558e14552
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-08-31 18:29:40 +0600

    nav added

--- src/Layout/Nav/Nav.jsx
+++ src/Layout/Nav/Nav.jsx
@@ -1,11 +1,127 @@
-import React from 'react';
+import React from "react";
+import "./Nav.css";
+import Logo from "../../assets/logo.png";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from "@nextui-org/navbar";
+import { Link } from "react-router-dom";
 
-const Nav = () => {
-    return (
-        <div>
-            <p>nav</p>
-        </div>
-    );
-};
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-export default Nav;
+  const menuItems = ["Home", "About", "Projects", "Events", "Blogs", "Contact"];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} id="font">
+      <NavbarContent justify="start">
+        <NavbarBrand>
+          <a href="#" id="logo">
+            <div className="flex items-center gap-x-2 sm:gap-x-3 md:gap-x-4">
+              <div>
+                <img
+                  src={Logo}
+                  alt=""
+                  className="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16"
+                />
+              </div>
+              <div className="text-white">
+                <p
+                  className="font-extrabold text-xl sm:text-2xl md:text-2xl lg:text-2xl text-[#4a7c59] m-0 leading-tight"
+                  id="logo-font"
+                >
+                  NATION AID
+                </p>
+                <p className="font-light text-md sm:text-lg md:text-xl lg:text-xl text-[#9cc069] m-0 leading-tight">
+                  FOUNDATION
+                </p>
+              </div>
+            </div>
+          </a>
+        </NavbarBrand>
+
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden px-5 text-[#4a7c59] font-bold"
+        />
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-x-8" justify="center">
+        <NavbarItem isActive>
+          <Link
+            href="#"
+            className="text-gray-400 font-medium hover:text-[#4a7c59] hover:border-b-2 hover:pb-2 hover:border-[#4a7c59]"
+          >
+            About
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            className="text-gray-400 font-medium hover:text-[#4a7c59] hover:border-b-2 hover:pb-2 hover:border-[#4a7c59]"
+            href="#"
+          >
+            Projects
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            className="text-gray-400 font-medium hover:text-[#4a7c59] hover:border-b-2 hover:pb-2 hover:border-[#4a7c59]"
+            href="#"
+          >
+            Events
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            className="text-gray-400 font-medium hover:text-[#4a7c59] hover:border-b-2 hover:pb-2 hover:border-[#4a7c59]"
+            href="#"
+          >
+            Blogs
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            className="text-gray-400 font-medium hover:text-[#4a7c59] hover:border-b-2 hover:pb-2 hover:border-[#4a7c59]  "
+            href="#"
+          >
+            Contact
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-x-16" justify="end">
+        <NavbarItem>
+          <Link
+            className="text-white font-medium px-4 py-2 bg-[#4a7c59] hover:border-2 hover:border-[#4a7c59] hover:bg-[#9cc069]"
+            href="#"
+          >
+            donete
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu
+        className="bg-black
+      "
+      >
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              // color={
+              //   index === 2 ? "black" : index === menuItems.length -1 ? "red" : "green"
+              // }
+              className="w-full hover:text-emerald-700 text-[#4a7c59] font-medium "
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
